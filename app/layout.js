ce80ee2c86c3d7b736b95caba13995fb937b5659
Roman Castro <xxromanxxtm@gmail.com>
2025-07-20 20:37:45 -0300

    seo

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,37 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "BACAN",
   description: "Bacan, jugar, reir, bacan",
+  metadataBase: new URL("https://bacan-romancastro.vercel.app/"),
+  openGraph: {
+    title: "BACAN",
+    description: "Bacan, jugar, reir, bacan",
+    url: "https://bacan-romancastro.vercel.app/",
+    images: [
+      {
+        url: '/og-image.jpg',
+        width: 1200,
+        height: 630,  
+        alt: "Captura de pantalla del sitio",  
+      }
+    ],
+    locale: "es_AR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "BACAN",
+    description: "Bacan, jugar, reir, bacan",
+    images: ["/og-image.jpg"],  
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  }
 };
 
 export default function RootLayout({ children }) {
---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://bacan-romancastro.vercel.app/", // ⬅️ Cambiá por tu dominio
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
