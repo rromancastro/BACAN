25d1f8fde603b2d3f36a04c75d4b3519db8d0880
Roman Castro <xxromanxxtm@gmail.com>
2025-08-12 18:09:38 -0300

    arreglo transicion de imagenes fithsection

--- app/sections/FifthSection.js
+++ app/sections/FifthSection.js
@@ -28,13 +28,11 @@ export const FifthSection = () => {
             <div style={{backgroundColor: 'transparent'}} id="fifthSectionCard">
                 {width >= 500 ? <h2 id="fifthSectionH2">¿DONDE<br />CONSIGO MI<br />BACAN?</h2> : <h2 id="fifthSectionH2">¿DONDE CONSIGO<br />MI BACAN?</h2>}
                 <a id="fifthSectionA" href="#">Quiero mi mazo</a>
-                {
-                    mostrarImagen === 1 ? <Image className="fifthSectionImage" src={`/fifthSection/banner1${width <= 500 ? 'mobile' : ''}.png`} alt="Imagen 1" width={1184} height={526} /> :
-                    mostrarImagen === 2 ? <Image className="fifthSectionImage" src={`/fifthSection/banner2${width <= 500 ? 'mobile' : ''}.png`} alt="Imagen 2" width={1184} height={526} /> :
-                    mostrarImagen === 3 ? <Image className="fifthSectionImage" src={`/fifthSection/banner3${width <= 500 ? 'mobile' : ''}.png`} alt="Imagen 3" width={1184} height={526} /> :
-                    mostrarImagen === 4 ? <Image className="fifthSectionImage" src={`/fifthSection/banner4${width <= 500 ? 'mobile' : ''}.png`} alt="Imagen 4" width={1184} height={526} /> :
-                    mostrarImagen === 5 ? <Image className="fifthSectionImage" src={`/fifthSection/banner5${width <= 500 ? 'mobile' : ''}.png`} alt="Imagen 5" width={1184} height={526} /> : null
-                }
+                <Image className="fifthSectionImage" src={`/fifthSection/banner1${width <= 500 ? 'mobile' : ''}.png`} alt="Imagen 1" width={1184} height={526} style={{opacity: '1'}}/>
+                <Image className="fifthSectionImage" src={`/fifthSection/banner2${width <= 500 ? 'mobile' : ''}.png`} alt="Imagen 2" width={1184} height={526} style={{opacity: mostrarImagen === 2 ? '1' : '0'}}/>
+                <Image className="fifthSectionImage" src={`/fifthSection/banner3${width <= 500 ? 'mobile' : ''}.png`} alt="Imagen 3" width={1184} height={526} style={{opacity: mostrarImagen === 3 ? '1' : '0'}}/>
+                <Image className="fifthSectionImage" src={`/fifthSection/banner4${width <= 500 ? 'mobile' : ''}.png`} alt="Imagen 4" width={1184} height={526} style={{opacity: mostrarImagen === 4 ? '1' : '0'}}/>
+                <Image className="fifthSectionImage" src={`/fifthSection/banner5${width <= 500 ? 'mobile' : ''}.png`} alt="Imagen 5" width={1184} height={526} style={{opacity: mostrarImagen === 5 ? '1' : '0'}}/>
             </div>
         </section>
     )
